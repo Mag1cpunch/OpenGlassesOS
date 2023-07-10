c21f601f89512c08f97210a99e8960c0d06524ac
gLiTcH#6948 <99803083+Mag1cpunch@users.noreply.github.com>
2023-07-10 04:40:14 -0700

    Update system.lua

--- system.lua
+++ system.lua
@@ -1,5 +1,11 @@
 -- AR Googles OS
 -- Requires Advanced Peripherals and CC: Tweaked
 
--- Check if AR Controller is available
-local controller = peripheral.find("arController")
+-- Required modules
+local ar = peripheral.find("arController")
+
+function openglassesos()
+	ar.clear()
+	ar.drawString("Welcome to OpenGlassesOS by Meteor!", -10, 10, 255, 147, 0)
+end
+openglassesos()
