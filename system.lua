ddffe66bc5ed5c831695fc2a4efd83bfddcd7085
gLiTcH#6948 <99803083+Mag1cpunch@users.noreply.github.com>
2023-07-10 04:23:46 -0700

    Update system.lua

--- system.lua
+++ system.lua
@@ -1,105 +1,61 @@
---[[
-ARC: AR Client
-Uses Plethora overlay glasses and modems to display 3D signage transmitted from local "beacons"
-]]
+-- AR Googles OS
+-- Requires Advanced Peripherals and CC: Tweaked
 
-local m = peripheral.find "modem"
-local mods = peripheral.wrap "back"
+-- Required modules
+local ap = peripheral.find("ap_ar_goggles")
 
-if not m then error "Modem required." end
-if not mods then error "Is this even on a neural interface?" end
-if not mods.canvas3d then error "Overlay glasses required." end
-
-local canv = mods.canvas3d()
-
-local object_channel = 666
-
-m.open(object_channel)
-
-local x, y, z
-
--- convert position to string for use as table key
-local function serialize_position(p)
-	return string.format("%f,%f,%f", p[1], p[2], p[3])
+-- Function to display the main menu
+local function displayMainMenu()
+  term.clear()
+  term.setCursorPos(1, 1)
+  print("AR Googles OS")
+  print("1. View Ore Locations")
+  print("2. Enable/Disable X-Ray Vision")
+  print("3. Exit")
+  write("Enter your choice: ")
 end
 
--- Generate a string representation of a table, for easy comparison. Not really hashing, I guess.
-local function hash_table(x)
-	if type(x) == "table" then
-		local out = ""
-		for k, v in pairs(x) do
-			if type(k) ~= "string" or not k:match "^_" then -- ignore keys beginning with _
-				out = out .. hash_table(k) .. ":" .. hash_table(v) .. ";"
-			end
-		end
-		return out
-	else
-		return tostring(x)
-	end
+-- Function to view ore locations
+local function viewOreLocations()
+  term.clear()
+  term.setCursorPos(1, 1)
+  print("Ore Locations:")
+  local locations = ap.getOreLocations()
+  if #locations == 0 then
+    print("No ore locations found.")
+  else
+    for i, location in ipairs(locations) do
+      print(i .. ". " .. location.x .. ", " .. location.y .. ", " .. location.z)
+    end
+  end
+  write("Press any key to continue...")
+  os.pullEvent("key")
 end
 
--- honestly not that elegant, but it works
--- TODO: do this more efficiently/nicely somehow?
-local function tables_match(x, y)
-	return hash_table(x) == hash_table(y)
+-- Function to toggle X-Ray vision
+local function toggleXRayVision()
+  ap.toggleXRayVision()
+  print("X-Ray vision toggled.")
+  sleep(1)
 end
 
-local objects = {}
-local timers = {}
-
-local function redraw(object)
-	local frame = object._frame
-	frame.clear()
+-- Main function to handle user input and execute commands
+local function main()
+  while true do
+    displayMainMenu()
+    local choice = tonumber(read())
+    if choice == 1 then
+      viewOreLocations()
+    elseif choice == 2 then
+      toggleXRayVision()
+    elseif choice == 3 then
+      break
+    else
+      print("Invalid choice. Please try again.")
+      sleep(1)
+    end
+  end
 end
 
-local function process_object(object)
-	local pos = serialize_position(object.position)
-	if objects[pos] then
-		if not tables_match(object, objects[pos]) then
-			print("redrawing", pos)
-			objects[pos] = object
-			redraw(objects[pos])
-		end
-		local t = os.startTimer(20)
-		timers[t] = pos
-	else
-		print("new object at", pos)
-		if x then
-			local frame = 
-		else
-			print("GPS error, cannot create object")
-		end
-	end
-end
-
-local function main_loop()
-	while true do
-		local event, timer, channel, reply_channel, message, distance = os.pullEvent()
-		if event == "modem_message" and channel == object_channel and distance and type(message) == "table" then -- ensure message is from this dimension and otherwise valid
-			for _, object in pairs(msg) do
-				local ok, err = pcall(process_object, object)
-				if not ok then printError(err) end
-			end
-		elseif event == "timer" then
-			local objpos = timers[timer]
-			if objpos then
-				local obj = objects[objpos]
-				if obj then
-					print(objpos, "timed out")
-					obj._frame.remove()
-					objects[objpos] = nil
-				end
-			end
-		end
-	end
-end
-
--- Request location every second
-local function GPS_loop()
-	while true do
-		x, y, z = gps.locate()
-		sleep(1)
-	end
-end
-
-parallel.waitForAll(GPS_loop, main_loop)
+-- Run the main function
+main()
