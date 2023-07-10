c9c0b49a257b3208daf93513bd846b296d8b0a84
gLiTcH#6948 <99803083+Mag1cpunch@users.noreply.github.com>
2023-07-10 08:42:44 -0700

    Update system.lua

--- system.lua
+++ system.lua
@@ -1,11 +1,15 @@
--- AR Googles OS
--- Requires Advanced Peripherals and CC: Tweaked
-
--- Required modules
-local ar = peripheral.find("arController")
-
-function openglassesos()
-	ar.clear()
-	ar.drawString("Welcome to OpenGlassesOS by Meteor!", -10, 10, 255, 147, 0)
+local controller = peripheral.find("arController") -- Finds the peripheral if one is connected
+ 
+if controller == nil then error("arController not found") end
+ 
+controller.setRelativeMode(true, 1600, 900) -- Convenient Aspect ratio for most screens
+while true do
+  local timer = os.startTimer(1)
+  local event, id
+  repeat
+    event, id = os.pullEvent("timer")
+  until id == timer
+  controller.clear() -- If you don't do this, the texts will draw over each other
+  controller.drawRightboundString(os.date(), -10, 10, 0xffffff)
+  controller.drawRightboundString("Welcome to OpenGlassesOS by Meteor!", -650, 10, 0xff8a57)
 end
-openglassesos()
